0d87622e98f17be524416a928b9c7ce80c4c7285
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2023-12-01 23:25:54 +0300

    123

---
+++ src/php/actions/check_new_version.php
@@ -0,0 +1,37 @@
+<?php
+
+$result = array_fill_keys(['newVersionNumber','newVersionLink','whatsNew'], '');
+try {
+    include_once dirname(__FILE__) . '/../common.php';
+
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $webtlo->release_api,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CONNECTTIMEOUT => 40,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_TIMEOUT => 40,
+        CURLOPT_USERAGENT => 'web-TLO'
+    ]);
+    $response = curl_exec($ch);
+    if ($response === false) {
+        Log::append('CURL ошибка: ' . curl_error($ch));
+        throw new Exception('Невозможно связаться с api.github.com');
+    }
+    $responseHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    $infoFromGitHub = $responseHttpCode == 200 ? json_decode($response, true) : false;
+
+    if (empty($infoFromGitHub)) {
+        throw new Exception('Что-то пошло не так при попытке получить актуальную версию с GitHub');
+    }
+
+    $result['newVersionNumber'] = $infoFromGitHub['name'];
+    $result['newVersionLink']   = $infoFromGitHub['zipball_url'];
+    $result['whatsNew']         = $infoFromGitHub['body'];
+} catch (Exception $e) {
+    Log::append($e->getMessage());
+}
+$result['log'] = Log::get();
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
